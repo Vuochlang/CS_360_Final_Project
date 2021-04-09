d9ecdb5eac525827ac9bac762c253eadbabbdcf3
Vuochlang <72162972+Vuochlang@users.noreply.github.com>
2021-04-08 18:52:45 -0700

    more updates
    finished the 'ls' command from client and execute locally without going to the server, output 20 lines at a time

--- code/mftp.c
+++ code/mftp.c
@@ -1,6 +1,8 @@
 #include "mftp.h"
 
 void createClient();
+void lsCommand();
+void show(char* pathName);
 
 bool debug = false;
 char portNumber[20];
@@ -68,32 +70,103 @@ void createClient() {
         // read from command line
         while ((readResult = read(0, buffer, max)) > 0) {
             if (strncmp("exit", buffer, 4) == 0) {
-                printf("Command string = 'exit'\n");
-                printf("Exit command encountered\n");
+                if (debug) {
+                    printf("Command string = 'exit'\n");
+                    printf("Exit command encountered\n");
+                }
                 bzero(buffer, max);
 
                 write(socketfd, "Q", 1);
-                printf("Awaiting server response\n");
+                if (debug)  printf("Awaiting server response\n");
                 while ((readResult = read(socketfd, buffer, max)) > 0) {
-                    if (strcmp(buffer, "A") == 0) {
+                    if (strcmp(buffer, "A") == 0 && debug) {
                         printf("Received server response: '%s'\n", buffer);
                         printf("Client exiting normally\n");
                         return;
                     }
+                    return;
                 }
             }
-            else {
+            else if (strncmp("ls", buffer, 2) == 0) {
+                printf("Command string = %s", buffer);
+                lsCommand();
+            }
+            else if (strcmp("rls", buffer) == 0) {
+                printf("Command string = %s\n", buffer);
                 write(socketfd, buffer, strlen(buffer));
+                // get server responses
+                while ((readResult = read(socketfd, buffer, max)) > 0) {
+                    write(1, buffer, readResult);
+                    break;
+                }
+            }
+            else if(strncmp("cd" , buffer, 2) == 0) {
+                printf("Command string = %s\n", buffer);
+            }
+            else {
+                printf("Command string = %s\n", buffer);
+                write(socketfd, buffer, strlen(buffer));
+                // get server responses
+                while ((readResult = read(socketfd, buffer, max)) > 0) {
+                    write(1, buffer, readResult);
+                    break;
+                }
             }
 
-            bzero(buffer, max);
-            // get server responses
-            while ((readResult = read(socketfd, buffer, max)) > 0) {
-                write(1, buffer, readResult);
-                break;
+            bzero(buffer, max); // empty buffer
+            write(1, prompt, strlen(prompt));  // print the prompt
+        }
+    }
+}
+
+void lsCommand() {
+    int pid = fork();
+
+    if (pid != 0) {
+        printf("Client parent waiting on child process %d to run ls locally\n", pid);
+        wait(&pid);
+        return;
+    }
+    else {
+        printf("Client child process %d executing local ls | more\n", getpid());
+
+        int fd[2], reader, writer;
+        assert(pipe(fd) >= 0);
+        reader = fd[0];
+        writer = fd[1];
+
+        printf("Child process %d starting more\n", getpid());
+        int pid2 = fork();
+        if (pid2 != 0) { // parent
+            close(writer);
+
+            // connect reader and take output from child
+            dup2(reader, STDIN_FILENO);
+            close(reader);
+            wait(&pid2);
+
+            char *arg[] = {"more", "-20", NULL};
+            char *command = "more";
+
+            if (execvp(command, arg) < 0) {
+                printf("%s\n", strerror(errno));
             }
-            bzero(buffer, max);
-            write(1, prompt, strlen(prompt));
+            printf("Error occurred during 'execvp'\n");
+        }
+        else {
+            printf("Child process %d starting ls\n", getpid());
+
+            // child process, run the 'ls -l' and redirect the output to stdin
+            close(reader);
+            dup2(writer, STDOUT_FILENO);  // connect stdout as the writer
+            close(writer);
+
+            char *arg[] = {"ls", "-l", NULL};
+            char *command = "ls";
+            if (execvp(command, arg) < 0) {
+                printf("%s\n", strerror(errno));
+            }
+            printf("Error occurred during 'execvp'\n");
         }
     }
 }
